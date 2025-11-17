6fc763d08c9d594539bbf10f6b65c138c9e40553
Ara Sadoyan <ara.sadoyan@netangels.net>
2025-11-17 20:21:52 +0100

    gitea

---
+++ init/gitea.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+apt-get update
+apt-get install -y wget bash curl net-tools
+
+wget -O /etc/profile.d/install.sh  -q  https://raw.githubusercontent.com/PetroSky-Cloud/One-click-app/refs/heads/main/gitea.sh
+
+curl -s https://raw.githubusercontent.com/PetroSky-Cloud/One-click-app/refs/heads/main/init/setpassword.sh | bash
