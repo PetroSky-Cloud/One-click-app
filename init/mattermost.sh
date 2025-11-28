059f1726355c3b88d6ffee4221024ee179d977c4
Ara Sadoyan <ara.sadoyan@netangels.net>
2025-11-28 13:37:49 +0100

    mattermost

--- init/mattermost.sh
+++ init/mattermost.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-apt-get update
-apt-get install -y wget bash curl net-tools
+apt update
+apt install -y wget bash curl net-tools
 
 wget -O /etc/profile.d/install.sh  -q  https://raw.githubusercontent.com/PetroSky-Cloud/One-click-app/refs/heads/main/mattermost.sh
 
